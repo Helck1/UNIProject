73f9509f0c229eca10ee1e1b7443ad00b55dc129
Helck <155485688+Helck1@users.noreply.github.com>
2025-06-19 11:37:15 +0000

    3.3 в процессе

--- scripts/script.js
+++ scripts/script.js
@@ -18,3 +18,39 @@ document.addEventListener("DOMContentLoaded", () => {
     intensiveImg.addEventListener('mouseenter', () => {
             console.log('Мышка наведена на изображение, показываем текст');
         });
+/* 2. Создание слайдера */
+let currentIndex = 0; //индекс карточек
+const slider = document.querySelectionAll(".reviews__item");
+const prevButton = document.querySelector(".reviews__left");
+const nextButton = document.querySelector(".reviews__right");
+const visibleCards = 2; //количество отображаемых карточек
+updateSlider();
+
+prevButton.addEventListener("click", () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex = slider.length - visibleCards; //переход к последним карточкам
+    }
+    updateSlider();
+});
+
+nextButton.addEventListener("click", () => {
+    if (currentIndex < slider.length - visibleCards) {
+        currentIndex++;
+    } else {
+        currentIndex = 0; // Переход к началу карточек
+    }
+    updateSlider ();
+});
+
+function updateSlider() {
+    slider.forEach((item, index) => {
+        // Проверяем нужно ли показывать карточку
+        if (index >= currentIndex && index < currentIndex + visibleCards) {
+            item.style.display = "block"; // Показываем карточку
+        } else {
+            item.style.display = "none"; //Скрываем карточку
+        }
+    });
+}
